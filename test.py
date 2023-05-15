4884334e3850c53e6950eb089f4e3cd2f6e42f65
Ayub <sir.kokabi@gmail.com>
2023-05-15 23:00:07 +0330

    b6

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-from typing import List
 from typing import Tuple
+from typing import List
+
 
 def add_numbers(numbers: List[int]) -> int:
     return sum(numbers)
 
+
 def get_user_info() -> Tuple[str, int, str]:
-    name = 'Alice'
+    name = "Alice"
     age = 30
-    occupation = 'Engineer'
+    occupation = "Engineer"
     return (name, age, occupation)
