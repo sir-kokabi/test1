62d6e5c93ca191b5840d42f365488afe61a737d3
Ayub Kokabi <sir.kokabi@gmail.com>
2023-05-21 13:41:02 +0330

    Update and rename test.py to app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def sum_two_numbers(a, b):
+    """Return the sum of two numbers.
+
+    Examples:
+
+    >>> sum_two_numbers(3, 4)
+    7
+    >>> sum_two_numbers(-2, 5)
+    3
+    >>> sum_two_numbers(0, 0)
+    0
+    """
+    return a + b
+
+d=f"Hello, world! "
