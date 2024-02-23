40329e22ef67fde0d58d86f91fe59a55be501b22
Ayub <sir.kokabi@gmail.com>
2024-02-23 20:56:29 +0330

    sample load base 64 font (works)

--- src/content.js
+++
@@ -1,6 +0,0 @@
-import cssText from "./style.css"
-
-export const config = {
-  matches: ["https://www.plasmo.com/*"]
-}
-
---
+++ src/contents/index.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+export const config = {
+  matches: ["https://twitter.com/*","https://x.com/*","https://www.plasmo.com/*","https://stackoverflow.com/*"],
+}
+
+
+
